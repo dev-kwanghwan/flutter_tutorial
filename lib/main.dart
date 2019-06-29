3b78f49a202437b1addbed8c9091c3729a743e89
william.cho <william.cho@kakaomobility.com>
2019-06-29 22:01:27 +0900

    theme 중 primary color를 흰색으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white
+      ),
       home: RandomWords(),
     );
   }
