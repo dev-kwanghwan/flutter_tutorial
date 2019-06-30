e313e7a3fce6025690e03838ea72b0aa2c184685
william.cho <william.cho@kakaomobility.com>
2019-06-30 16:39:53 +0900

    Flutter  학습하기 관련 프로젝트를 테스트해볼 수 있도록 메인화면 개선과 기존에 작업해놨던 WriteYour First Flutter App codelabs 패키지로 분리함

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
+import 'codelabs/codelab1.dart';
 
 void main() => runApp(MyApp());
 
-// #docregion MyApp
 class MyApp extends StatelessWidget {
-  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.white
-      ),
-      home: RandomWords(),
+      title: 'Flutter 학습하기',
+      theme: ThemeData(primaryColor: Colors.black),
+      home: MyWidget(),
     );
   }
-// #enddocregion build
 }
-// #enddocregion MyApp
 
-// #docregion RWS-var
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  // #enddocregion RWS-var
-
-  // #docregion _buildSuggestions
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider();
-          /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  // #enddocregion _buildSuggestions
-
-  // #docregion _buildRow
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  // #enddocregion _buildRow
-
-  // #docregion RWS-build
+class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
+        title: Text('Flutter 학습하기'),
       ),
-      body: _buildSuggestions(),
+      body: RaisedButton(
+          child: Text("WriteYourFirstFlutterApp Button"),
+          onPressed: () {
+            moveWriteYourFirstFlutterApp(context);
+          }),
     );
   }
 
-// #enddocregion RWS-build
-// #docregion RWS-var
-
-  void _pushSaved() {
-    Navigator.of(context).push(MaterialPageRoute<void>(
-      builder: (BuildContext context) {
-        final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
-          return ListTile(
-            title: Text(pair.asPascalCase, style: _biggerFont),
-          );
-        });
-        final List<Widget> divided =
-            ListTile.divideTiles(context: context, tiles: tiles).toList();
-
-        return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided));
-      },
-    ));
+  void moveWriteYourFirstFlutterApp(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => WriteYourFirstFlutterAppRoute()));
   }
-}
-// #enddocregion RWS-var
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
 }
