ce9aa5a7d903ce686100d35d09690ae34df78076
william.cho <william.cho@kakaomobility.com>
2019-07-04 01:38:47 +0900

    add send button & apply theme

--- lib/codelabs/codelab2.dart
+++ lib/codelabs/codelab2.dart
@@ -41,19 +41,32 @@ class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: _buildTextComposer(),
+      body: _buildTextComposer(context),
     );
   }
-  
-  Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(hintText: "Send a message")
-      )
-    );
+
+  Widget _buildTextComposer(BuildContext context) {
+    return new IconTheme(data: new IconThemeData(color: Theme
+        .of(context)
+        .accentColor), child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+                child: new TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration: new InputDecoration.collapsed(
+                        hintText: "Send a message")
+                )),
+            new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: new IconButton(icon: new Icon(Icons.send),
+                    onPressed: () => _handleSubmitted(_textController.text))
+            )
+          ],
+        )
+    ));
   }
 
   void _handleSubmitted(String text) {
