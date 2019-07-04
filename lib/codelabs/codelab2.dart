010ad67e2d93233b9bca6c346001112edd366a71
william.cho <william.cho@kakaomobility.com>
2019-07-04 23:51:51 +0900

    Add a UI for displaying messages

--- lib/codelabs/codelab2.dart
+++ lib/codelabs/codelab2.dart
@@ -36,40 +36,89 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: _buildTextComposer(context),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+              child: new ListView.builder(
+            padding: new EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          )),
+          new Divider(
+            height: 1.0,
+          ),
+          new Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(context),
+          )
+        ],
+      ),
+
+//      body: _buildTextComposer(context),
     );
   }
 
   Widget _buildTextComposer(BuildContext context) {
-    return new IconTheme(data: new IconThemeData(color: Theme
-        .of(context)
-        .accentColor), child: new Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new Row(
-          children: <Widget>[
-            new Flexible(
-                child: new TextField(
-                    controller: _textController,
-                    onSubmitted: _handleSubmitted,
-                    decoration: new InputDecoration.collapsed(
-                        hintText: "Send a message")
-                )),
-            new Container(
-                margin: new EdgeInsets.symmetric(horizontal: 4.0),
-                child: new IconButton(icon: new Icon(Icons.send),
-                    onPressed: () => _handleSubmitted(_textController.text))
-            )
-          ],
-        )
-    ));
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(
+                    child: new TextField(
+                        controller: _textController,
+                        onSubmitted: _handleSubmitted,
+                        decoration: new InputDecoration.collapsed(
+                            hintText: "Send a message"))),
+                new Container(
+                    margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                    child: new IconButton(
+                        icon: new Icon(Icons.send),
+                        onPressed: () =>
+                            _handleSubmitted(_textController.text)))
+              ],
+            )));
   }
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+}
+
+const String _name = "Y";
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(_name, style: Theme.of(context).textTheme.subhead),
+            new Container(
+                margin: const EdgeInsets.only(top: 5.0), child: new Text(text))
+          ],
+        ));
   }
 }
