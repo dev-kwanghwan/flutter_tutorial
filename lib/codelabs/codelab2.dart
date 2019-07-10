4cdb1490af51c34052caed38dc00470a5bf9b96c
william.cho <william.cho@kakaomobility.com>
2019-07-10 22:53:56 +0900

    text 비비어있는지 확인해서 전송버튼 활성여부 체크하기

--- lib/codelabs/codelab2.dart
+++ lib/codelabs/codelab2.dart
@@ -32,6 +32,7 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
 
   @override
   Widget build(BuildContext context) {
@@ -40,17 +41,19 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
         children: <Widget>[
           new Flexible(
               child: new ListView.builder(
-            padding: new EdgeInsets.all(8.0),
-            reverse: true,
-            itemBuilder: (_, int index) => _messages[index],
-            itemCount: _messages.length,
-          )),
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              )),
           new Divider(
             height: 1.0,
           ),
           new Container(
             decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor,
+              color: Theme
+                  .of(context)
+                  .cardColor,
             ),
             child: _buildTextComposer(context),
           )
@@ -70,7 +73,9 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
 
   Widget _buildTextComposer(BuildContext context) {
     return new IconTheme(
-        data: new IconThemeData(color: Theme.of(context).accentColor),
+        data: new IconThemeData(color: Theme
+            .of(context)
+            .accentColor),
         child: new Container(
             margin: const EdgeInsets.symmetric(horizontal: 8.0),
             child: new Row(
@@ -78,21 +83,31 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
                 new Flexible(
                     child: new TextField(
                         controller: _textController,
+                        onChanged: (String text) {
+                          setState(() {
+                            _isComposing = text.length > 0;
+                          });
+                        },
                         onSubmitted: _handleSubmitted,
                         decoration: new InputDecoration.collapsed(
                             hintText: "Send a message"))),
                 new Container(
                     margin: new EdgeInsets.symmetric(horizontal: 4.0),
                     child: new IconButton(
-                        icon: new Icon(Icons.send),
-                        onPressed: () =>
-                            _handleSubmitted(_textController.text)))
+                      icon: new Icon(Icons.send),
+                      onPressed: _isComposing ? () =>
+                          _handleSubmitted(_textController.text)
+                          : null,
+                    ))
               ],
             )));
   }
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
     ChatMessage message = new ChatMessage(
       text: text,
       animationController: new AnimationController(
@@ -128,15 +143,20 @@ class ChatMessage extends StatelessWidget {
                 margin: const EdgeInsets.only(right: 16.0),
                 child: new CircleAvatar(child: new Text(_name[0])),
               ),
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(_name, style: Theme.of(context).textTheme.subhead),
-                  new Container(
-                    margin: const EdgeInsets.only(top: 5.0),
-                    child: new Text(text),
-                  ),
-                ],
+              new Expanded(
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(_name, style: Theme
+                        .of(context)
+                        .textTheme
+                        .subhead),
+                    new Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: new Text(text),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
