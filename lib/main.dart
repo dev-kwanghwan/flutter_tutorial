d14829e161e5225478a9bfd18fe2a02ca220f4e8
william.cho <william.cho@kakaomobility.com>
2019-07-10 23:11:41 +0900

    OS별 theme 분기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 import 'codelabs/codelab1.dart';
 import 'codelabs/codelab2.dart';
@@ -6,48 +7,59 @@ import 'codelabs/codelab2.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final ThemeData kIOSTheme = new ThemeData(
+      primarySwatch: Colors.orange,
+      primaryColor: Colors.grey[100],
+      primaryColorBrightness: Brightness.light);
+
+  final ThemeData kDefaultTheme = new ThemeData(
+    primarySwatch: Colors.purple,
+    accentColor: Colors.orangeAccent[400],
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter 학습하기',
-      theme: ThemeData(primaryColor: Colors.black),
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: MyWidget(),
     );
   }
-
-
 }
 
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter 학습하기'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          RaisedButton(
-              child: Text("WriteYourFirstFlutterApp Button"),
-              onPressed: () {
-                moveWriteYourFirstFlutterAppRoute(context);
-              }),
-          RaisedButton(
-              child: Text("BuildingBeautifulUIsWithFlutterRoute Button"),
-              onPressed: () {
-                moveBuildingBeautifulUIsWithFlutterRoute(context);
-              }),
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text('Flutter 학습하기'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            RaisedButton(
+                child: Text("WriteYourFirstFlutterApp Button"),
+                onPressed: () {
+                  moveWriteYourFirstFlutterAppRoute(context);
+                }),
+            RaisedButton(
+                child: Text("BuildingBeautifulUIsWithFlutterRoute Button"),
+                onPressed: () {
+                  moveBuildingBeautifulUIsWithFlutterRoute(context);
+                }),
+          ],
+        ));
   }
 
   void moveWriteYourFirstFlutterAppRoute(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => WriteYourFirstFlutterAppRoute()));
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => WriteYourFirstFlutterAppRoute()));
   }
 
   void moveBuildingBeautifulUIsWithFlutterRoute(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => BuildingBeautifulUIsWithFlutterRoute()));
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => BuildingBeautifulUIsWithFlutterRoute()));
   }
 }
